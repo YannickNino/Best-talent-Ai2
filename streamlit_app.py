4e8c75e59367097f7d21f0a0d10f8369a5d5aa46
YannickNino <yannickaristide.jlg@gmail.com>
2025-02-13 12:02:01 +0100

    second change

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,21 @@
 import streamlit as st
 import pandas as pd
-import math
-from pathlib import Path
+import numpy as np
+import os
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+#import math
+#from pathlib import Path
 
-# Set the title and favicon that appear in the Browser's tab bar.
-st.title('My loveling  App')
+
+os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"]
+
+prompt = st.text_input("enter your prompt :")
+
+
+#def get_model():
+#prompt_template
+
+@st.cache_data
+def generate_response(prompt):
+    return "Hello world" + prompt
